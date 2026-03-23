d6830549d23323af5aa6f6a583beb2ae695eae52
olz <olz@G301-CC0191.local>
2026-03-23 19:07:37 +0800

    feat: add eslint rules

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,17 +1,56 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import nextEslintPluginNext from "@next/eslint-plugin-next";
+import nx from "@nx/eslint-plugin";
+import baseConfig from "../../eslint.config.mjs";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
+  { plugins: { "@next/next": nextEslintPluginNext } },
+  ...baseConfig,
+  ...nx.configs["flat/react-typescript"],
+  {
+    files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
+    rules: {
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          selector: "variableLike",
+          format: ["camelCase", "PascalCase"],
+        },
+        {
+          selector: "function",
+          format: ["camelCase", "PascalCase"],
+        },
+        {
+          selector: "typeLike",
+          format: ["PascalCase"],
+        },
+      ],
+      "@typescript-eslint/no-explicit-any": "error",
+      complexity: ["error", 4],
+      "max-lines": [
+        "error",
+        {
+          max: 150,
+          skipBlankLines: true,
+          skipComments: true,
+        },
+      ],
+      "max-lines-per-function": [
+        "error",
+        {
+          max: 180,
+          skipBlankLines: true,
+          skipComments: true,
+        },
+      ],
+    },
+  },
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
+    ".open-next/**",
     "out/**",
-    "build/**",
-    "next-env.d.ts",
+    "dist/**",
+    "**/out-tsc",
   ]),
 ]);
 
