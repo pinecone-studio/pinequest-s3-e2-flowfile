538992469e40fa873dc53cbe99d0c6d0b7f0fdcf
olz <olz@G301-CC0191.local>
2026-03-30 19:03:31 +0800

    feat(api): add session attempt expiry submit flow

--- api/src/modules/session/session.module.ts
+++ api/src/modules/session/session.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'src/database/database.module';
 import { SessionController } from './session.controller';
 import { SessionService } from './session.service';
+import { NotificationModule } from '../notification/notification.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, NotificationModule],
   controllers: [SessionController],
   providers: [SessionService],
   exports: [SessionService],
