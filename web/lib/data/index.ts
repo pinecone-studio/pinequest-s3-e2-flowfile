69cab739e33db83bfea295f8870b3d50932dc1c6
bolormau <bolormau@gmail.com>
2026-04-01 10:40:17 +0900

    feat(web): implement data access layer for local persistence (#476)

---
+++ web/lib/data/index.ts
@@ -0,0 +1,29 @@
+export {
+  getAll,
+  getById,
+  remove,
+  save,
+  update,
+} from './local-storage'
+
+export {
+  CURRENT_ADMIN_ID,
+  CURRENT_STUDENT_ID,
+  CURRENT_TEACHER_ID,
+  getCurrentAdmin,
+  getCurrentStudent,
+  getCurrentTeacher,
+  initializeData,
+  initialAttempts,
+  initialClasses,
+  initialCourses,
+  initialExamAssignments,
+  initialExams,
+  initialQuestions,
+  initialSchedules,
+  initialSubjects,
+  initialSubmissions,
+  initialUsers,
+} from './seed-init'
+
+export { initialResults } from './seed-results'
